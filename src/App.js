54bb7d35b7fa963a7d8dbfa2c49d06c4177fd1ec
Yohan Reyes <yohanreyes@outlook.com>
2021-06-21 11:06:56 -0400

    Mobile stateless design completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from "react";
 import Slider from "./Slider";
 import checkIcon from './images/icon-check.svg'
-
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-        <h1>Simple, traffic-based pricing</h1>
-        <h2> Sign-up for our 30-day trial. No credit card required.  </h2>
+      <section className='intro'>
+        <h1 className='main-header-text'>  Simple, traffic-based pricing</h1>
+        <h2 className='main-descp'> Sign-up for our 30-day trial. No credit card required.  </h2>
+        </section>
         <Slider />
          <section className='features'> 
-         <div>
-           <img src= {checkIcon} alt='Check'/>
+         <div className='feature'>
+           <img className='check-icon' src= {checkIcon} alt='Check'/>
            Unlimited websites</div>
-         <div>
-         <img src= {checkIcon} alt='Check'/>
+         <div className='feature'>
+         <img className='check-icon' src= {checkIcon} alt='Check'/>
          100% data ownership</div>
-         <div>
-         <img src= {checkIcon} alt='Check'/>
+         <div className='feature'>
+         <img className='check-icon' src= {checkIcon} alt='Check'/>
          Email reports</div>
             <button className='btn start-trial'>Start my trial</button>
          </section>
--- src/Slider.js
+++ src/Slider.js
@@ -1,30 +1,29 @@
 import React from 'react'
 import sliderIcon from './images/icon-slider.svg'
+import './Slider.css'
 
 const Slider = () => {
     let pagesview = 100;
-    let amount = 16;
+    let amount = 16.00;
     // Make those stateful
     return (
         <div id='slider'>
-            <h3> {pagesview + 'K'}Pageviews</h3>
+            <h3 className='pageviews'> {pagesview + 'K'} Pageviews</h3>
             <div className='slider-bar'>
-                <div className='slider-empty'> </div>
-                <div> <img src={sliderIcon} alt='slider'/> </div>
-                <div className='slider-full'> </div>
+               <div className='slider-empty'> 
+                  <div className='slider-full'> </div>
+               </div>
+                <div className='slider-icon-container'> <img className='slider-icon' src={sliderIcon} alt='slider'/> </div>
             </div>
-            <text> 
-            ${amount}/month
+            <text className='amount'> 
+            <strong className='amount-strong'>${amount}</strong>/month
             </text>
-
             <div className= 'monthly-yearly'>
              <text>Monthly Billing</text> 
             <div className='choise-outside'> 
              <div className='inner-choise'> 
                  </div>  
-               
                   </div>
-            Yearly Billing
             <text>  Yearly Billing</text> 
              <div className='discount'>-25% {/*25% discount text should be used when on desktop view*/}</div>
             </div>
